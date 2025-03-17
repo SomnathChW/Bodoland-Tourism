e554a78ac77fd8fcb7e4313853617bb7a12e902c
SomnathChW <somnathchowdhury.0100@gmail.com>
2025-03-18 00:53:11 +0530

    Setting Up Appwrite

---
+++ lib/appwrite.tsx
@@ -0,0 +1,29 @@
+import { Platform } from "react-native";
+import { Client, Databases } from "react-native-appwrite";
+
+if (
+    !process.env.EXPO_PUBLIC_ENDPOINT ||
+    !process.env.EXPO_PUBLIC_PROJECT_ID ||
+    !process.env.EXPO_PUBLIC_ANDROID_PLATFORM ||
+    !process.env.EXPO_PUBLIC_IOS_PLATFORM
+) {
+    console.error("Please set the environment variables in .env file");
+}
+
+const appwrite_endpoint = process.env.EXPO_PUBLIC_ENDPOINT;
+const appwrite_project = process.env.EXPO_PUBLIC_PROJECT_ID;
+const appwrite_android_platform = process.env.EXPO_PUBLIC_ANDROID_PLATFORM;
+const appwrite_ios_platform = process.env.EXPO_PUBLIC_IOS_PLATFORM;
+
+const client = new Client()
+    .setEndpoint(appwrite_endpoint)
+    .setProject(appwrite_project);
+
+switch (Platform.OS) {
+    case "ios":
+        client.setPlatform(appwrite_android_platform);
+        break;
+    case "android":
+        client.setPlatform(appwrite_ios_platform);
+        break;
+}
