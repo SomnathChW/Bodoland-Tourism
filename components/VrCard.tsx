b2b9116c6278733fe5a5691d6b75d19f54424592
SomnathChW <somnathchowdhury.0100@gmail.com>
2025-03-31 20:50:11 +0530

    Restructure and VR Page
    Restructured the folders and started work on VR Page

---
+++ components/VrCard.tsx
@@ -0,0 +1,164 @@
+import {
+    StyleSheet,
+    Text,
+    View,
+    Image,
+    Dimensions,
+    Pressable,
+} from "react-native";
+import React from "react";
+import { useRouter } from "expo-router";
+import { Entypo, MaterialCommunityIcons } from "@expo/vector-icons";
+
+type Props = {
+    item: {
+        identifier: string;
+        image: any;
+        title?: string;
+        location?: string;
+        geohash?: string;
+    };
+    index: number;
+};
+
+const { width, height } = Dimensions.get("screen");
+const PADDING = 15;
+const GAP = 15;
+const NUM_CARDS_ON_SCREEN = 2;
+const CARD_WIDTH =
+    (width - PADDING * 2 - GAP * (NUM_CARDS_ON_SCREEN - 1)) /
+    NUM_CARDS_ON_SCREEN;
+const CARD_HEIGHT = height * 0.25;
+
+const VrCard = ({ item, index }: Props) => {
+    const router = useRouter();
+    return (
+        <View style={index % 2 === 0 ? styles.cardLeft : styles.cardRight}>
+            <Pressable
+                onPress={() =>
+                    router.navigate({
+                        pathname: "/details",
+                        params: { identifier: item.identifier },
+                    })
+                }
+            >
+                <Image source={{ uri: item.image }} style={styles.image} />
+                {item.title && (
+                    <View style={styles.infoView}>
+                        <View style={styles.textView}>
+                            <Text style={styles.title}>{item.title}</Text>
+                            <View
+                                style={{
+                                    flexDirection: "row",
+                                    alignItems: "center",
+                                }}
+                            >
+                                <Entypo
+                                    name="location-pin"
+                                    size={12}
+                                    color="#646f7e"
+                                    style={[styles.icon, { marginLeft: 2 }]}
+                                />
+                                <Text style={styles.location}>
+                                    {item.location}
+                                </Text>
+                            </View>
+                        </View>
+                        <MaterialCommunityIcons
+                            name="google-cardboard"
+                            size={30}
+                            color="#646f7e"
+                            style={styles.icon}
+                        />
+                    </View>
+                )}
+                {item.geohash && (
+                    <View style={styles.distanceCard}>
+                        <Text
+                            style={{
+                                fontFamily: "SfProMedium",
+                                fontSize: 10,
+                                color: "white",
+                                textAlign: "center",
+                            }}
+                        >
+                            {item.geohash}
+                        </Text>
+                    </View>
+                )}
+            </Pressable>
+        </View>
+    );
+};
+
+export default VrCard;
+
+const styles = StyleSheet.create({
+    cardLeft: {
+        width: CARD_WIDTH,
+        height: CARD_HEIGHT,
+        backgroundColor: "rgba(52, 52, 52, 0.35)",
+        borderRadius: 10,
+        shadowOffset: { width: 10, height: 0 },
+        marginVertical: 10,
+        marginLeft: PADDING,
+        marginRight: GAP / 2,
+        overflow: "hidden",
+    },
+    cardRight: {
+        width: CARD_WIDTH,
+        height: CARD_HEIGHT,
+        backgroundColor: "rgba(52, 52, 52, 0.35)",
+        borderRadius: 10,
+        shadowOffset: { width: 10, height: 0 },
+        marginVertical: 10,
+        marginLeft: GAP / 2,
+        marginRight: PADDING,
+        overflow: "hidden",
+    },
+    distanceCard: {
+        position: "absolute",
+        alignItems: "center",
+        justifyContent: "center",
+        height: CARD_HEIGHT * 0.12,
+        top: CARD_HEIGHT * 0.05,
+        left: CARD_WIDTH * 0.05,
+        alignSelf: "flex-start",
+        padding: 5,
+        paddingHorizontal: 10,
+        borderRadius: 10,
+        backgroundColor: "rgba(52, 52, 52, 0.5)",
+    },
+    image: {
+        width: CARD_WIDTH,
+        height: CARD_HEIGHT * 0.75,
+    },
+    infoView: {
+        width: CARD_WIDTH,
+        height: CARD_HEIGHT * 0.25,
+        justifyContent: "space-between",
+        flexDirection: "row",
+        alignItems: "center",
+        paddingHorizontal: 10,
+    },
+    textView: {
+        flex: 1,
+        justifyContent: "center",
+    },
+    title: {
+        fontFamily: "SfProMedium",
+        fontSize: 14,
+        fontWeight: "bold",
+        color: "white",
+        marginHorizontal: 5,
+    },
+    location: {
+        fontFamily: "SfProMedium",
+        fontSize: 12,
+        color: "#646f7e",
+        marginHorizontal: 2,
+    },
+    icon: {
+        marginLeft: 10,
+    },
+});
--- pages/Home.tsx
+++ pages/Home.tsx
@@ -4,17 +4,17 @@ import { useState } from "react";
 import { Ionicons } from "@expo/vector-icons";
 import { useRouter } from "expo-router";
 
-import Section from "@/components/Section";
+import Section from "@/components/UI/Section";
 import CardVertical from "@/components/CardVertical";
 import CardHorizontal from "@/components/CardHorizontal";
-import Carousel from "@/components/Carousel";
-import CardSquare from "@/components/CardSquare";
+import Carousel from "@/components/UI/Carousel/Carousel";
+import CardSquare from "@/components/CategoryCard";
 
 import { carouselData } from "@/data/slider_data";
 import { districtData } from "@/data/district_data";
 import { categoryData } from "@/data/category_data";
 import { useAuth } from "@/context/AuthContext";
-import AlertDialog from "@/components/AlertDialog";
+import AlertDialog from "@/components/UI/AlertDialog";
 
 const Home = () => {
     const router = useRouter();
@@ -61,6 +61,7 @@ const Home = () => {
                     style={styles.scrollPadding}
                     contentContainerStyle={{ paddingBottom: 20 }}
                     stickyHeaderIndices={[1]}
+                    showsVerticalScrollIndicator={false}
                 >
                     <Carousel itemList={carouselData} />
                     <Section
